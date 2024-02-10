ebd398142b66bb0060db94299ce421116f25a856
felipe <felipeoliveirasites@gmail.com>
2024-02-10 19:37:13 -0300

    Create index.php

---
+++ desafios/d003b/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    //copiado
+    $cotação = 5.17;
+
+    //quando você tem
+    $real = 1000;
+
+    //equivalência em dólar
+    $dólar = $real / $cotação;
+        echo "Seu R$$real equivale a US$$dólar";
+
+    ?>
+</body>
+</html>
